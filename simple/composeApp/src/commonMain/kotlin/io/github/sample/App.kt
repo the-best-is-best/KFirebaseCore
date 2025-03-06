ad50577c8d72516c7a44bd50791b1b1b92084b8c
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-03-06 22:39:29 +0200

    - **iosApp:** Added `DEVELOPMENT_TEAM` to `project.pbxproj` to ensure consistent team settings across collaborators, resolving potential code signing issues. - **composeApp:** Added functionality to display the Firebase project ID within the app, improving visibility of Firebase configuration. - **composeApp:** Added a button to display the Project ID. - **composeApp:** Displayed the Project ID using `Text`.

--- simple/composeApp/src/commonMain/kotlin/io/github/sample/App.kt
+++ simple/composeApp/src/commonMain/kotlin/io/github/sample/App.kt
@@ -1,5 +1,6 @@
 package io.github.sample
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,10 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,19 +22,22 @@ import io.github.sample.theme.AppTheme
 
 @Composable
 internal fun App() = AppTheme {
+    var projectId by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .windowInsetsPadding(WindowInsets.safeDrawing)
             .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
        ElevatedButton(
            onClick = {
-               println(KFirebaseCore.app().options.projectId)
+               projectId = KFirebaseCore.app().options.projectId ?: "No Project ID"
            }
        ){
            Text("Print Project ID")
        }
+        Text(text = projectId)
     }
 }
