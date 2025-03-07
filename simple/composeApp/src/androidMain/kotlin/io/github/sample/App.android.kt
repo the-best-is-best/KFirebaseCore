771494bd17f514b5fffec50a02b5d59c02e70f9d
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-03-07 23:30:11 +0200

    Feat: Initialize AndroidKFirebaseCore in AppActivity

--- simple/composeApp/src/androidMain/kotlin/io/github/sample/App.android.kt
+++ simple/composeApp/src/androidMain/kotlin/io/github/sample/App.android.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.firebase_core.AndroidKFirebaseCore
+import io.github.firebase_core.KFirebaseCore
 
 class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidKFirebaseCore.initialization(this)
         enableEdgeToEdge()
         setContent { App() }
     }
